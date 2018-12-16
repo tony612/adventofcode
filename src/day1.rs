184e4448cacbc22d1e3102625ab1c5181b9a21b2
Tony Han <h.bing612@gmail.com>
2018-12-16 11:40:19 +0800

    finish day2-1

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,47 @@
 use std::fs;
+use std::collections::HashMap;
 
 pub fn run() {
+  part1();
+  part2();
+}
+
+fn part1() {
     let body = fs::read_to_string("./input/day1-1.txt").expect("couldn't read the input file");
 
-    let lines = body.lines();
     let mut result = 0;
-    for line in lines {
+    for line in body.lines() {
         // println!("{}", line);
         let number: i32 = line.parse().unwrap();
         result += number;
+    }
+    println!("Result is: {}", result);
+}
+
+fn part2() {
+    let body = fs::read_to_string("./input/day1-1.txt").expect("couldn't read the input file");
+
+    let mut result = 0;
+    let mut sum = 0;
+    let mut sums: HashMap<i32, bool> = HashMap::new();
+    loop {
+        for line in body.lines() {
+            // println!("{}", line);
+            let number: i32 = line.parse().unwrap();
+            sum += number;
+            let found = sums.get(&sum);
+            match found {
+                Some(_) => {
+                    result = sum;
+                    break;
+                },
+                None =>
+                    sums.insert(sum, true),
+            };
+        }
+        if result != 0 || sum == 0 {
+            break;
+        }
     }
     println!("Result is: {}", result);
 }
--- src/day2.rs
+++ src/day2.rs
@@ -1,31 +1,36 @@
 use std::fs;
-use std::collections::HashMap;
 
 pub fn run() {
-    let body = fs::read_to_string("./input/day1-1.txt").expect("couldn't read the input file");
+    let body = fs::read_to_string("./input/day2-1.txt").expect("couldn't read the input file");
 
     let lines = body.lines();
-    let mut result = 0;
-    let mut sum = 0;
-    let mut sums: HashMap<i32, bool> = HashMap::new();
-    loop {
-        for line in lines.clone() {
-            // println!("{}", line);
-            let number: i32 = line.parse().unwrap();
-            sum += number;
-            let found = sums.get(&sum);
-            match found {
-                Some(_) => {
-                    result = sum;
-                    break;
-                },
-                None =>
-                    sums.insert(sum, true),
-            };
-        }
-        if result != 0 || sum == 0 {
-            break;
+    let mut two = 0;
+    let mut three = 0;
+    for line in lines {
+        let mut chars: Vec<char> = line.chars().collect();
+        chars.sort();
+        // check at last
+        chars.push('\0');
+        let mut curr_char = '\0';
+        let mut curr_count = 0;
+        let mut has_two = false;
+        let mut has_three = false;
+        for b in chars.iter() {
+            if *b == curr_char {
+                curr_count += 1;
+            } else {
+                if !has_two && curr_count == 2 {
+                    has_two = true;
+                    two += 1;
+                }
+                if !has_three && curr_count == 3 {
+                    has_three = true;
+                    three += 1;
+                }
+                curr_count = 1;
+                curr_char = *b;
+            }
         }
     }
-    println!("Result is: {}", result);
+    println!("Result is {} * {} = {}", two, three, two * three);
 }
