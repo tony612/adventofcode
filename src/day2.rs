7ddfcb26c14f9e5b3cba2feddd915f2b6d05ef5b
Tony Han <h.bing612@gmail.com>
2018-12-16 21:34:14 +0800

    finish day2

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,11 @@
 use std::fs;
 
 pub fn run() {
+    part1();
+    part2();
+}
+
+fn part1() {
     let body = fs::read_to_string("./input/day2-1.txt").expect("couldn't read the input file");
 
     let lines = body.lines();
@@ -33,4 +38,41 @@ pub fn run() {
         }
     }
     println!("Result is {} * {} = {}", two, three, two * three);
+}
+
+fn part2() {
+    let body = fs::read_to_string("./input/day2-1.txt").expect("couldn't read the input file");
+    let lines: Vec<&str> = body.lines().collect();
+    let len = lines.len();
+    let mut diff = 100;
+    let mut common: Vec<char> = vec!();
+
+    for i in 0..len {
+        for j in (i+1)..len {
+            match id_diff(lines[i], lines[j]) {
+                (curr_diff, curr_common) =>
+                    if curr_diff < diff {
+                        diff = curr_diff;
+                        common = curr_common;
+                    }
+            }
+        }
+    }
+    let common = common.iter().collect::<String>();
+    println!("Result is {} {:?}", diff, common);
+}
+
+fn id_diff(str1: &str, str2: &str) -> (u32, Vec<char>) {
+    let chars1 = str1.chars();
+    let chars2 = str2.chars();
+    let mut diff: u32 = 0;
+    let mut common: Vec<char> = vec!();
+    for (c1, c2) in chars1.zip(chars2) {
+        if c1 == c2 {
+            common.push(c1);
+        } else {
+            diff += 1;
+        }
+    }
+    (diff, common)
 }
